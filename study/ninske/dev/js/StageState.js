756a44bdcaa06b585a8b14a23712f44c37460e52
naosim <nao_pillows@yahoo.co.jp>
2015-12-09 08:17:41 +0900

    キャラクターをジャンプさせれる

--- study/ninske/dev/js/StageState.js
+++ study/ninske/dev/js/StageState.js
@@ -33,11 +33,11 @@ var WALL = [
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 1, 0, 0, 0, 1],	
+  [1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
 ]
 
 var createNinsukeSprite = function() {
-  var ninsukeSprite = game.add.sprite(31, 0, 16, 24);
+  var ninsukeSprite = game.add.sprite(31, 0, 'ninsuke_hitarea');
   game.physics.arcade.enable(ninsukeSprite);
   ninsukeSprite.outOfBoundsKill = true;
   ninsukeSprite.ay = 0;// 加速度
@@ -63,9 +63,10 @@ var createNinsukeSprite = function() {
     ninsukeSprite.y += ninsukeSprite.vy;
     ninsukeSprite.x += ninsukeSprite.vx;
   }
-  
-  var ninsukeImageSprite = ninsukeSprite.addChild(game.make.sprite((ninsukeSprite.width - 32) / 2, ninsukeSprite.height - 32, 'ninsuke'));
+
+  var ninsukeImageSprite = ninsukeSprite.addChild(game.make.sprite((ninsukeSprite.width - 32) / 2, ninsukeSprite.height - 32, 'ninsuke', 0));
   game.camera.follow(ninsukeSprite);
+  ninsukeSprite.ninsukeImageSprite = ninsukeImageSprite;
   return ninsukeSprite;
 }
 
@@ -79,37 +80,42 @@ var StageState = function() {
   function createTextButton(x, y, text, style, onClickAction) {
     var result = game.add.text(x, y, text, style);
     result.anchor.set(0.5);
-    
+
     // テキストと同じサイズでボタンを作る
     var button = game.add.button(result.x, result.y, null/*画像無し*/, onClickAction, this, 2, 1, 0);
     button.width = result.width;
     button.height = result.height;
     button.anchor.set(0.5);
     game.world.moveDown(button);
-    
+
     return {
       text: result,
       button: button
     };
   }
-  
+
   function createWall(x, y) {
 	  var wall = game.add.sprite(x, y, 'wall');
 	  wall.height = wall.width * 3;
 	  return wall;
   }
-  
+
   function create() {
-	game.world.setBounds(0, 0, WALL[0].length * 32, WALL.length * 32 * 3);
-	game.physics.startSystem(Phaser.Physics.ARCADE);
-	cursors = game.input.keyboard.createCursorKeys();
-	
+  	game.world.setBounds(0, 0, WALL[0].length * 32, WALL.length * 32 * 3);
+    game.stage.backgroundColor = '#ffffdd';
+  	game.physics.startSystem(Phaser.Physics.ARCADE);
+  	cursors = game.input.keyboard.createCursorKeys();
+
     wallGroup = createWallGroup();
     ninsukeSprite = createNinsukeSprite();
+
+    // var button = game.add.button(0, 0, null, onJump);
+    // button.width = game.world.width;
+    // button.height = game.world.height;
   }
-   
+
   var jumpPressed = false;
-  
+
   // ジャンプ
   function onJump() {
     console.log('onjump', ninsukeState.key);
@@ -118,7 +124,7 @@ var StageState = function() {
       ninsukeSprite.ay = GRAVITY;
       ninsukeSprite.vy = JUMP_VY;
       ninsukeSprite.y--;
-      
+
       // x方向
       if(ninsukeState.isLeft) {
         console.log('jumpToRight');
@@ -131,16 +137,14 @@ var StageState = function() {
       jumpPressed = true;
     }
   }
-  
+
   function onHitWall(_, wall) {
     console.log(wall.name);
   }
-  
+
   function update() {
-	  if(cursors.up.isDown) {
-		  onJump();
-	  }
-	  
+    game.input.onDown.add(onJump, this);
+
 	  // ニンスケの状態判定
 	  ninsukeState = NinsukeState.free;
 	  var onHitWall = function(_, wall) {
@@ -152,25 +156,29 @@ var StageState = function() {
 			  ninsukeSprite.x = wall.x - ninsukeSprite.width + 1;// fixme
 		  }
 	  };
-      
+
 	  // ジャンプした瞬間はあたり判定しない(離陸できないため)
 	  if(!jumpPressed) {
 		  game.physics.arcade.overlap(ninsukeSprite, wallGroup, onHitWall, null, this);
 	  } else {
 		  jumpPressed = false;
 	  }
-	  
+
 	  // 状態によって加速度等を変える
 	  if(ninsukeState.isOnWall && !ninsukeState.isOnGround) {
-          ninsukeSprite.move.slideWall();
+      ninsukeSprite.move.slideWall();
 	  } else if(ninsukeState.isOnWall && ninsukeState.isOnGround) {
-          ninsukeSprite.move.stop();
+      ninsukeSprite.move.stop();
 	  } else {
-          ninsukeSprite.move.fall();
+      ninsukeSprite.move.fall();
 	  }
-	  
 	  ninsukeSprite.step();
-		  
+
+    if(ninsukeState.isLeft || ninsukeSprite.vx > 0) {
+      ninsukeSprite.ninsukeImageSprite.frame = 0;
+    } else if(ninsukeState.isRight || ninsukeSprite.vx < 0) {
+      ninsukeSprite.ninsukeImageSprite.frame = 1;
+    }
   }
 
   return {
@@ -185,8 +193,6 @@ var StageState = function() {
     preload: function(){},
     create: create,
     update: update,
-    render: function(){}
+    render: function(){game.debug.text(ninsukeSprite.ninsukeImageSprite.frame, 32, 32);}
   };
 };
-
-
--- study/ninske/dev/js/StartState.js
+++ study/ninske/dev/js/StartState.js
@@ -1,10 +1,10 @@
 var StartState = function() {
   var global;
-  function create() {    
+  function create() {
     var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
     var text = game.add.text(game.world.centerX, game.world.centerY, "Start", style);
     text.anchor.set(0.5);
-    
+
     // テキストと同じサイズでボタンを作る
     var button = game.add.button(text.x, text.y, null/*画像無し*/, onClick, this, 2, 1, 0);
     button.width = text.width;
@@ -12,7 +12,7 @@ var StartState = function() {
     button.anchor.set(0.5);
     game.world.moveDown(button);
   }
- 
+
   function onClick() {
     game.state.start('main', true, false, global);
   }
@@ -22,11 +22,14 @@ var StartState = function() {
       global = _global;
     },
     preload: function(){
-      game.load.image('ninsuke', '../../../common/img/block.png', 32, 32);
-      game.load.image('wall', '../../../common/img/block_min.png');
+      game.load.image('ninsuke_hitarea', '../../../common/img/ninsuke_hitarea.png');
+      game.load.spritesheet('ninsuke', '../../../common/img/ninsuke2.png', 32, 32);
+      game.load.image('wall', '../../../common/img/block_min_3.png');
     },
     create: create,
-    update: function(){},
+    update: function(){
+      game.input.onDown.add(onClick, this);
+    },
     render: function(){}
   };
 };
